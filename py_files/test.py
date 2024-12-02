1c242386e80aa1805bdfec3965d3f666bf2790cf
Fedor Belov <fedor.belov.45@yandex.ru>
2024-12-02 15:15:53 +0300

    хфввъ

--- py_files/test.py
+++ py_files/test.py
@@ -1,5 +1,23 @@
-for x in range(0, 2):
-    for y in range(0, 2):
-        for z in range(0, 2):
-            f = not((x <= y) == (not (x and y) and z))
-            print(x, y, z, f)
+import numpy as np
+import matplotlib.pyplot as plt
+from sympy import *
+
+target = 1
+
+r = abs(target)
+phi = np.angle(target)
+
+n = 8
+roots = [r ** (1 / 8) * np.exp(1j * (phi + 2 * np.pi * k) / n) for k in range(n)]
+
+x = [z.real for z in roots]
+y = [z.imag for z in roots]
+
+plt.figure(figsize=(6, 6))
+plt.axhline(0, color='black', linewidth=0.5, linestyle='-')
+plt.axvline(0, color='black', linewidth=0.5, linestyle='-')
+plt.scatter(x, y, color="red")
+plt.grid(color="gray", linestyle="-", linewidth=0.5)
+plt.xlabel("Re")
+plt.ylabel("Im")
+plt.show()
